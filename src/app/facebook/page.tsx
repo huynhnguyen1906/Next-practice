d47b15f42c546899e68d1726a0f8a6c5be8ac939
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-06-12 14:27:50 +0900

    chore: Update npm dependencies for bootstrap and react-bootstrap

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -1,3 +1,23 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { Button } from 'react-bootstrap';
 export default function Facebook() {
-    return <div>Facebook</div>;
+    const router = useRouter();
+    const handleNavigation = () => {
+        router.push('/');
+    };
+    return (
+        <div>
+            <div>
+                <button
+                    onClick={() => {
+                        handleNavigation();
+                    }}
+                >
+                    Back Home
+                </button>
+                <Button variant="primary">Facebook</Button>
+            </div>
+        </div>
+    );
 }
