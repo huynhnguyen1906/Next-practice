f6af96575b232d39b58818f414f93872b675dd94
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-06-24 12:12:21 +0900

    chore: use useSWR in tiktok/page.tsx and render data from api

--- src/app/youtube/page.tsx
+++ src/app/youtube/page.tsx
@@ -1,10 +1,22 @@
 'use client';
 import { useRouter } from 'next/navigation';
+import axiosInstance from '@/lib/axiosInstance';
+import useSWR from 'swr';
+const fetcher = (url: string) => axiosInstance.get(url).then((res) => res.data);
+
 export default function Youtube() {
     const router = useRouter();
     const handleNavigation = () => {
         router.push('/');
     };
+
+    const { data, error } = useSWR('/blogs', fetcher, {
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+    });
+
+    console.log(data);
     return (
         <div>
             Youtube
@@ -15,6 +27,22 @@ export default function Youtube() {
             >
                 Back Home
             </button>
+            <div>
+                {data ? (
+                    <div>
+                        {data.map((item: any, index: number) => {
+                            return (
+                                <div key={index}>
+                                    <h1>{item.title}</h1>
+                                    <p>{item.content}</p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </div>
         </div>
     );
 }
